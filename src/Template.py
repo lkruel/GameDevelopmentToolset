12efa12254db56d7dcdb22cd92a185aab94fa117
` <steveb@stevencburrichter.com>
2016-02-20 14:02:08 -0800

    Added more things I typically use in Template.py

--- src/Template.py
+++ src/Template.py
@@ -25,3 +25,44 @@ def init(nodes):
 
         #Accessing point 0's position attribute
         print node.geometry().points[0].attribValue('P')
+
+        #Creating nodes
+        subnet = node.createOutputNode('subnet', 'TemplateSubnetTest')
+        node1 = subnet.createNode('xform', 'TemplateXFormTest')
+        node2 = subnet.createNode('null', 'TemplateNullTest')
+
+        #Connecting two nodes
+        node2.setFirstInput(node1)
+        #Alternatively, you can directly set a specific input: node2.setInput(0, node1)
+
+        #Laying out nodes
+        #This command will set ALL children...previously made nodes WILL be moved :)
+        subnet.layoutChildren()
+
+        #Setting a bunch of parameters (setParms takes a dictionary input!)
+        node1.setParms({'tx':1,
+                        'ty':1,
+                        'tz':1})
+
+        #Setting keyframes
+        hou.keyed_parm = node1.parm('rx')
+        hou_keyframe = hou.Keyframe()
+        hou_keyframe(setFrame(1))
+        hou_keyframe.setValue(15)
+        hou_keyed_parm.setKeyframe(hou_keyframe)
+
+        hou.keyed_parm = node1.parm('ry')
+        hou_keyframe = hou.Keyframe()
+        hou_keyframe(setFrame(1))
+        hou_keyframe.setValue(30)
+        hou_keyed_parm.setKeyframe(hou_keyframe)
+
+        hou.keyed_parm = node1.parm('rz')
+        hou_keyframe = hou.Keyframe()
+        hou_keyframe(setFrame(1))
+        hou_keyframe.setValue(45)
+        hou_keyed_parm.setKeyframe(hou_keyframe)
+
+        #Evaluating Houdini variables
+        print "Timeline start frame: {}".format(hou.expandString('$RFSTART'))
+        print "Timeline end frame: {}".format(hou.expandString('$RFEND'))
